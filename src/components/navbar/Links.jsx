459cd9a92018084200917ba3d69ff004997dc4b9
Andres Avendano <anavguitar@gmail.com>
2024-10-01 15:00:47 -0700

    added src/app folders and jsx files

---
+++ src/components/navbar/Links.jsx
@@ -0,0 +1,27 @@
+const Links = () => {
+  
+    const links = [
+        {
+            title: "Homepage",
+            path: "/"
+        },
+        {
+            title: "About",
+            path: "/about"
+        },
+        {
+            title: "Contact",
+            path: "/contact"
+        },
+        {
+            title: "Blog",
+            path: "/posts"
+        }
+    ];
+  
+    return (
+    <div>Links</div>
+  )
+}
+
+export default Links;
